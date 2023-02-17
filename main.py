f68a50252ddf1ee882d08aedae65dc1ed693e52b
Brandon Ortiz <Brandonortiz7821@gmail.com>
2023-02-16 23:27:54 -0500

    °Switched 'users' from dict(list) to nested dict - i.e. dict(dict) °Added Search algorithm for user access [WIP]

--- main.py
+++ main.py
@@ -6,14 +6,16 @@
 # Create the dictionary for the user logins
 # ---------USE A DICT INSIDE A DICT-----------
 # noinspection SpellCheckingInspection
-users = {1: ['Mikey', 'pizzatime', 'standard'],
-         2: ['Donny', 'halfshell', 'standard'],
-         3: ['Ralph', 'shredhead', 'standard'],
-         4: ['Leo', 'cowabunga', 'admin']}
+users = {1: {'name': 'Mikey', 'pass': 'pizzatime', 'role': 'standard'},
+         2: {'name': 'Donny', 'pass': 'halfshell', 'role': 'standard'},
+         3: {'name': 'Ralph', 'pass': 'shredhead', 'role': 'standard'},
+         4: {'name': 'Leo', 'pass': 'cowabunga', 'role': 'admin'}
+         }
+
+for user in users:
+    print(users[user]['name'])
 # The user should be asked if they already have an account, or if they'd like to create a new one.
 verify = input(str("Are you an existing user? Y/N :"))
-
-print(verify)
 
 if verify.lower() == 'y':
     # If the user already has an account, they will be prompted to input their username and password
@@ -24,9 +26,14 @@ if verify.lower() == 'y':
     user_credentials = user_name + ' ' + user_pass
     # If the credentials are found in the dictionary, they will be granted access.
     for user in users:
-        if user
-
-    print(user_credentials)
+        if users[user]['name'] == str(user_name):
+            # Once logged, the user's role is determined. A welcome message should be displayed if they are a
+            # standard user.
+            print("You are a user, congrats!")
+            break
+            # A list of all user accounts should be displayed if the user is an Administrator.
+    else:
+        print("You are not a user!")
 elif verify.lower() == 'n':
     # If the user does not have an account, they will be prompted to enter a new username and password.
     new_acc_username = input("To create a new account please enter in a username : ")
@@ -37,5 +44,3 @@ elif verify.lower() == 'n':
 else:
     print("Error - Invalid Input!")
 
-# Once logged, the user's role is determined. A welcome message should be displayed if they are a standard user.
-# A list of all user accounts should be displayed if the user is an Administrator.
