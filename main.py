5ff2185786094fd2cb68e94d21902258e81528aa
Brandon Ortiz <Brandonortiz7821@gmail.com>
2023-02-18 23:03:38 -0500

    ° Added contingency for when user does not already have an account. ° Finalized project

--- main.py
+++ main.py
@@ -3,23 +3,17 @@
 # File Name  : H3_Login.py
 # Function   : This program will authenticate user access
 
-# Create the dictionary for the user logins
-# ---------USE A DICT INSIDE A DICT-----------
-# noinspection SpellCheckingInspection
 users = {1: {'name': 'Mikey', 'pass': 'pizzatime', 'role': 'standard'},
          2: {'name': 'Donny', 'pass': 'halfshell', 'role': 'standard'},
          3: {'name': 'Ralph', 'pass': 'shredhead', 'role': 'standard'},
          4: {'name': 'Leo', 'pass': 'cowabunga', 'role': 'admin'}
          }
 
-for user in users:
-    print(users[user]['name'])
 # The user should be asked if they already have an account, or if they'd like to create a new one.
 verify = input(str("Are you an existing user? Y/N :"))
 
 if verify.lower() == 'y':
     # If the user already has an account, they will be prompted to input their username and password
-
     user_name = input("Please enter in your User Name : ")
     user_pass = input("Please enter in your Password : ")
 
@@ -47,12 +41,11 @@ elif verify.lower() == 'n':
     # If the user does not have an account, they will be prompted to enter a new username and password.
     new_acc_username = input("Please enter in your desired user name: ")
     new_acc_pass = input("Please enter in your desired password: ")
-
     new_user = new_acc_username + ' ' + new_acc_pass
     print("Thank you, your information has been stored.")
-    print("\nACCESS GRANTED: Welcome! ")
-
 
     # This information will then be inserted and stored in the dictionary.
+    print("\nACCESS GRANTED: Welcome! ")
+    users.update({5: {'name': new_acc_username, 'pass': new_acc_pass, 'role': 'standard'}})
 else:
     print("Error - Invalid Input!")
